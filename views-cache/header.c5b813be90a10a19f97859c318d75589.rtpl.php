f401c8b7f888c925b0300db58ae9c4778ec5be1d
Flademir <fladermorais@hotmail.com>
2018-08-16 10:04:09 -0300

    Incluindo Classe Page

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Curso Hcode - Ecommerce</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
